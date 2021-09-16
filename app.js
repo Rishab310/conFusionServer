7b2b8d4da489fc2eedf2948948c84418f175a6ab
Rishab Goyal <rishabgoyal432@gmail.com>
2021-09-17 03:17:47 +0530

    Passports for authentication

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
-
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -45,6 +46,8 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -53,100 +56,20 @@ app.use('/users', usersRouter);
 function auth(req, res, next){
   console.log(req.session);
   
-  if (!req.session.user){
+  if (!req.user){
     var err = new Error('You are not authenticated');
     err.status = 403;
     return next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated');
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
 app.use(auth);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Using Cookies
 
-// function auth(req, res, next){
-//   console.log(req.signedCookies);
-  
-//   if (!req.signedCookies.user){
-//     var authHeader = req.headers.authorization;
-//     if (!authHeader){
-//       var err = new Error('You are not authenticated');
-//       res.setHeader('WWW-Authenticate','Basic');
-//       err.status = 401;
-//       return next(err);
-//     }
-//     var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
-//     var username = auth[0];
-//     var password = auth[1];
-
-//     if (username === 'admin' && password === 'password'){
-//       res.cookie('user','admin',{ signed: true})
-//       next();
-//     }
-//     else {
-//       var err = new Error('You are not authenticated');
-//       res.setHeader('WWW-Authenticate','Basic');
-//       err.status = 401;
-//       return next(err);
-//     }
-//   }
-//   else {
-//     if (req.signedCookies.user === 'admin') {
-//       next();
-//     }
-//     else {
-//       var err = new Error('You are not authenticated')
-//       res.setHeader('WWW-Authenticate','Basic');
-//       err.status = 401;
-//       return next(err);
-//     }
-//   }
-// }
-
-
-// Before cookies were setup
-// function auth(req, res, next){
-//   console.log(req.headers);
-
-//   var authHeader = req.headers.authorization;
-//   if (!authHeader){
-//     var err = new Error('You are not authenticated')
-//     res.setHeader('WWW-Authenticate','Basic');
-//     err.status = 401;
-//     return next(err);
-//   }
-//   var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
-//   var username = auth[0];
-//   var password = auth[1];
-
-//   if (username === 'admin' && password === 'password'){
-//     next();
-//   }
-//   else {
-//     res.setHeader('WWW-Authenticate','Basic');
-//     err.status = 401;
-//     return next(err);
-//   }
-
-  
-// }
-
-
-
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
@@ -167,3 +90,184 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+// Before using passports
+
+
+// var createError = require('http-errors');
+// var express = require('express');
+// var path = require('path');
+// var cookieParser = require('cookie-parser');
+// var logger = require('morgan');
+// var session = require('express-session');
+// var FileStore = require('session-file-store')(session);
+
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+// var dishRouter = require('./routes/dishRouter');
+// var promoRouter = require('./routes/promoRouter');
+// var leaderRouter = require('./routes/leaderRouter');
+
+// const mongoose = require('mongoose');
+
+// const Dishes = require('./models/dishes');
+// const { networkInterfaces } = require('os');
+
+// const url = "mongodb://localhost:27017/conFusion";
+// const connect = mongoose.connect(url);
+
+// connect.then((db) => {
+//   console.log("Connected Correctly to Server");
+// },(err) => {console.log(err);});
+
+
+// var app = express();
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// // app.use(cookieParser('12345-67890-09876-54321'));
+
+// app.use(session({
+//   name: 'session-id',
+//   secret: '12345-67890-09876-54321',
+//   saveUninitialized: false,
+//   resave: false,
+//   store: new FileStore()
+// }));
+
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+
+// function auth(req, res, next){
+//   console.log(req.session);
+  
+//   if (!req.session.user){
+//     var err = new Error('You are not authenticated');
+//     err.status = 403;
+//     return next(err);
+//   }
+//   else {
+//     if (req.session.user === 'authenticated') {
+//       next();
+//     }
+//     else {
+//       var err = new Error('You are not authenticated');
+//       err.status = 403;
+//       return next(err);
+//     }
+//   }
+// }
+
+// app.use(auth);
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// // Using Cookies
+
+// // function auth(req, res, next){
+// //   console.log(req.signedCookies);
+  
+// //   if (!req.signedCookies.user){
+// //     var authHeader = req.headers.authorization;
+// //     if (!authHeader){
+// //       var err = new Error('You are not authenticated');
+// //       res.setHeader('WWW-Authenticate','Basic');
+// //       err.status = 401;
+// //       return next(err);
+// //     }
+// //     var auth = new Buffer.from(authHeader.split(' ')[1],'base64').toString().split(':');
+// //     var username = auth[0];
+// //     var password = auth[1];
+
+// //     if (username === 'admin' && password === 'password'){
+// //       res.cookie('user','admin',{ signed: true})
+// //       next();
+// //     }
+// //     else {
+// //       var err = new Error('You are not authenticated');
+// //       res.setHeader('WWW-Authenticate','Basic');
+// //       err.status = 401;
+// //       return next(err);
+// //     }
+// //   }
+// //   else {
+// //     if (req.signedCookies.user === 'admin') {
+// //       next();
+// //     }
+// //     else {
+// //       var err = new Error('You are not authenticated')
+// //       res.setHeader('WWW-Authenticate','Basic');
+// //       err.status = 401;
+// //       return next(err);
+// //     }
+// //   }
+// // }
+
+
+// // Before cookies were setup
+// // function auth(req, res, next){
+// //   console.log(req.headers);
+
+// //   var authHeader = req.headers.authorization;
+// //   if (!authHeader){
+// //     var err = new Error('You are not authenticated')
+// //     res.setHeader('WWW-Authenticate','Basic');
+// //     err.status = 401;
+// //     return next(err);
+// //   }
+// //   var auth = new Buffer(authHeader.split(' ')[1],'base64').toString().split(':');
+// //   var username = auth[0];
+// //   var password = auth[1];
+
+// //   if (username === 'admin' && password === 'password'){
+// //     next();
+// //   }
+// //   else {
+// //     res.setHeader('WWW-Authenticate','Basic');
+// //     err.status = 401;
+// //     return next(err);
+// //   }
+
+  
+// // }
+
+
+
+
+// // app.use('/', indexRouter);
+// // app.use('/users', usersRouter);
+// app.use('/dishes', dishRouter);
+// app.use('/promotions', promoRouter);
+// app.use('/leaders', leaderRouter);
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
+
+// module.exports = app;
