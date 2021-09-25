54077da1a77186ad648a1687d5589eb2a557e58b
Rishab Goyal <rishabgoyal432@gmail.com>
2021-09-26 01:24:31 +0530

    Mongoose Population for comments and authors

--- models/dishes.js
+++ models/dishes.js
@@ -16,8 +16,8 @@ const commentSchema = new Schema(
       required: true,
     },
     author: {
-      type: String,
-      required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
     },
   },
   {
@@ -69,3 +69,93 @@ const dishSchema = new Schema(
 var Dishes = mongoose.model("Dish", dishSchema);
 
 module.exports = Dishes;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Simple schema without mongose population 
+
+
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
+// require('mongoose-currency').loadType(mongoose);
+// const Currency = mongoose.Types.Currency;
+
+// const commentSchema = new Schema(
+//   {
+//     rating: {
+//       type: Number,
+//       min: 1,
+//       max: 5,
+//       required: true,
+//     },
+//     comment: {
+//       type: String,
+//       required: true,
+//     },
+//     author: {
+//       type: String,
+//       required: true,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//     usePushEach: true
+//   }
+// );
+
+// const dishSchema = new Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     description: {
+//       type: String,
+//       required: true,
+//     },
+//     image: {
+//       type: String,
+//       required: true,
+//     },
+//     category: {
+//       type: String,
+//       required: true,
+//     },
+//     label: {
+//       type: String,
+//       default: "",
+//     },
+//     price: {
+//       type: Currency,
+//       required: true,
+//       min: 0
+//     },
+//     featured: {
+//       type: Boolean,
+//       default: false,
+//     },
+//     comments: [commentSchema],
+//   },
+//   {
+//     timestamps: true,
+//     usePushEach: true
+//   }
+// );
+
+// var Dishes = mongoose.model("Dish", dishSchema);
+
+// module.exports = Dishes;
