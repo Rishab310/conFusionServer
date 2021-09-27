63bf5e47bd944c10c4241ef4cb6c5c0a400e9d00
Rishab Goyal <rishabgoyal432@gmail.com>
2021-09-28 01:26:51 +0530

    verify admin function

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ module.exports = app;
 
 
 //  mongod --dbpath=data --bind_ip 127.0.0.1
+//  mongod --dbpath="D:\Web\Full Stack Web Developement\NodeJS\Project\mongodb\data" --bind_ip 127.0.0.1
 
 
 
